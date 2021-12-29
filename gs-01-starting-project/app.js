3e74fa7df5d6985e9e71efe7afb42f2a3fd8d91d
Ruben Alves <rubenanapu@hotmail.com>
2021-12-29 16:54:07 -0300

    Using Prettier (VSCode Plugin) to format JS code

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,17 +1,17 @@
 Vue.createApp({
-    data(){
-        return {
-            goals: [],
-            enteredValue: ''
-        }
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
     },
-    methods: {
-        addGoal(){
-            this.goals.push(this.enteredValue);
-            this.enteredValue = '';
-        }
-    }
-}).mount('#vueApp');
+  },
+}).mount("#vueApp");
 
 // const inputEl = document.querySelector('input');
 // const buttonEl = document.querySelector('button');
