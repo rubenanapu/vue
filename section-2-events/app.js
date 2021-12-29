6b0e4ee92cf20e085d430f5b600d8939bb119190
Ruben Alves <rubenanapu@hotmail.com>
2021-12-29 18:48:23 -0300

    Working with Event Arguments

--- section-2-events/app.js
+++ section-2-events/app.js
@@ -2,14 +2,15 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      incrementBy: 7,
     };
   },
   methods: {
-    incrementCounter() {
-      this.counter++;
+    incrementCounter(number) {
+      this.counter += number;
     },
-    decrementCounter() {
-      this.counter--;
+    decrementCounter(number) {
+      this.counter -= number;
     },
   },
 });
