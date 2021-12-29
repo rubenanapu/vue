8a19d1a94566438b0a5845b69c50db92fe9e38ea
Ruben Alves <rubenanapu@hotmail.com>
2021-12-29 17:51:32 -0300

    Calling Vue methods directly inside HTML

--- section-2-basics/app.js
+++ section-2-basics/app.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
         };
     },
     methods: {
-
+        outputGoal(){
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5){
+                return 'Learn Vue';
+            }
+            return 'Master Vue';
+        }
     }
 });
 
