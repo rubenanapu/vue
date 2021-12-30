459c2787aab92c14adddbabf9fc939e886534344
Ruben Alves <rubenanapu@hotmail.com>
2021-12-30 19:29:49 -0300

    Making Special Attack button work

--- section-4-project/app.js
+++ section-4-project/app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       enemyHealth: 100,
       playerHealth: 100,
+      round: 0,
     };
   },
   computed: {
@@ -16,6 +17,9 @@ const app = Vue.createApp({
     playerCss() {
       return { width: this.playerHealth + "%" };
     },
+    mayUseSpecialAttack() {
+      return this.round % 3 !== 0;
+    },
   },
   methods: {
     attackEnemy() {
@@ -24,8 +28,14 @@ const app = Vue.createApp({
       this.attackPlayer();
     },
     attackPlayer() {
+      this.round++;
       const damage = getRandom(8, 16);
       this.playerHealth -= damage;
+    },
+    specialAttack() {
+      const damage = getRandom(15, 25);
+      this.enemyHealth -= damage;
+      this.attackPlayer();
     },
   },
 });
