4a983865413c830542c715876e03badff30a8c6c
Ruben Alves <rubenanapu@hotmail.com>
2021-12-30 11:44:30 -0300

    Changing CSS Style using VueJS (:style, and camelCase properties)

---
+++ section-2-styling/app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      aSelected: false,
+      bSelected: false,
+      cSelected: false,
+    };
+  },
+  methods: {
+    boxSelected(box) {
+      this.aSelected = this.bSelected = this.cSelected = false;
+
+      if (box === "A") {
+        this.aSelected = true;
+      } else if (box === "B") {
+        this.bSelected = true;
+      } else if (box === "C") {
+        this.cSelected = true;
+      }
+    },
+  },
+});
+
+app.mount("#styling");
