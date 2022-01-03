0fb60992b8dfa526c024ce5fd2f0179d97da0879
Ruben Alves <rubenanapu@hotmail.com>
2022-01-03 20:18:08 -0300

    Converted "provide" into a function "provide()" to avoid having duplicated data (it now returns data from data(){})

--- section-8-component-communication-08-a-potential-problem/src/App.vue
+++ section-8-component-communication-08-a-potential-problem/src/App.vue
@@ -32,25 +32,12 @@ export default {
       activeTopic: null,
     };
   },
-  provide: {
+  provide() {
     // Data that can be easily accessible through children, grand-*-children, etc, not by parents
-    topics: [
-      {
-        id: 'basics',
-        title: 'The Basics',
-        description: 'Core Vue basics you have to know',
-        fullText:
-          'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
-      },
-      {
-        id: 'components',
-        title: 'Components',
-        description:
-          'Components are a core concept for building Vue UIs and apps',
-        fullText:
-          'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
-      },
-    ],
+    // Children use the "inject" property
+    return {
+      topics: this.topics,
+    };
   },
   methods: {
     activateTopic(topicId) {
