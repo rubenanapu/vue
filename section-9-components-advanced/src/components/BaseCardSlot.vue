b8c7966b58aa4d00d6939365c6ad11138fcf4742
Ruben Alves <rubenanapu@hotmail.com>
2022-01-05 20:07:49 -0300

    Using slots ( <slot></slot> ) when defining wrappers

---
+++ section-9-components-advanced/src/components/BaseCardSlot.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+// slots are used when we want a wrapper that can be easily reused in
+// different places to insert Vue code
+export default {};
+</script>
+
+<style scoped>
+div {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+</style>
--- section-9-components-advanced/src/main.js
+++ section-9-components-advanced/src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import BaseBadge from './components/BaseBadge.vue';
+import BaseCardSlot from './components/BaseCardSlot.vue';
 
 
 const app = createApp(App);
 
 
 app.component('base-badge', BaseBadge);
+app.component('base-card-slot', BaseCardSlot);
 
 app.mount('#app');
